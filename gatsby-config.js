26dad725a8cad8f6ff43754e06249755a8f00bc0
hsantos4 <hector.santos597@gmail.com>
2023-09-10 21:37:54 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Passion Fruit Media`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
